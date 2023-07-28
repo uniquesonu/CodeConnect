57ffa591004cc056b9745ba7f8d321de61f72eed
uniquesonu <21bcs10550@cuchd.in>
2023-07-29 01:59:09 +0530

    remove unused code

--- src/main.jsx
+++ src/main.jsx
@@ -6,17 +6,15 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-<<<<<<< HEAD
+
 import Landing from './Pages/Landing.jsx';
 import Dashboard from './Pages/Dashboard.jsx';
 import Signup from './Pages/Signup.jsx';
 import Login from './Pages/Login.jsx';
-<<<<<<< HEAD
-=======
+
 import Create from './Pages/Create.jsx';
 import NotesPrev from './Pages/NotesPrev.jsx';
->>>>>>> 5608d44 (data bases added)
-=======
+
 import Landing from './pages/Landing.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import Signup from './pages/Signup.jsx';
@@ -24,7 +22,7 @@ import Login from './pages/Login.jsx';
 import Create from './pages/Create.jsx';
 import NotesPrev from './pages/NotesPrev.jsx';
 import Drawer from './components/Drawer.jsx';
->>>>>>> 2b2d96a (added loader)
+
 
 const router = createBrowserRouter([
   {
